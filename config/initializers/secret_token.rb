5dec9c948930065017aaa0e6b9497c60fba18cbc
twinturbotom <tommmello@gmail.com>
2012-10-22 14:19:44 -0400

    initial with postrgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pwbc::Application.config.secret_token = '0bbb1a354770fd33a5798b737a8db5c5b58534d79465c5896212c4a43786b99d45aef3e665e009c54e5ac246e24ffc95942ade9760caf588db80cd9d09a46987'
