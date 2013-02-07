964877857f6ef234dd8b9ad5e0a661658c52afe1
twinturbotom <tommmello@gmail.com>
2013-02-07 09:01:03 -0500

    responsive

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Pwbc::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
-  config.assets.iitialize_on_precompile=false
+  config.assets.initialize_on_precompile=false
 
   # Generate digests for assets URLs
   config.assets.digest = true
